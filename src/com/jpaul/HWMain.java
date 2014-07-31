6fbf1d90d03529b4c199b109a4637a15210b18d5
Paul Ticlavilca <pticlavilca@gmail.com>
2014-07-31 17:57:54 -0500

    basic compile

---
+++ src/com/jpaul/HWMain.java
@@ -0,0 +1,11 @@
+package com.jpaul;
+
+/**
+ * Created by Usuario on 31/07/2014.
+ */
+public class HWMain {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
